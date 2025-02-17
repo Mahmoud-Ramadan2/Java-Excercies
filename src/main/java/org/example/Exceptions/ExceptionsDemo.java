bed51c80f40d5c4ea884b0607fa16f9f88c508c5
Mahmoud-Ramadan2 <mahmoudramadan385@gmail.com>
2025-02-17 14:58:41 +0200

    add IOException handller

--- src/main/java/org/example/Exceptions/ExceptionsDemo.java
+++ src/main/java/org/example/Exceptions/ExceptionsDemo.java
@@ -1,15 +1,32 @@
 package org.example.Exceptions;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Locale;
 
 public class ExceptionsDemo {
 
+
     public static void show(){
-sayHello(null);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("fllile.txt"));
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line =  bufferedReader.readLine();
+            }
+            bufferedReader.close();
+
+        }
+        catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+
     }
 
-    public static void sayHello(String name){
-        System.out.println("Hello "+name.toUpperCase());
-    }
+
 
 }
